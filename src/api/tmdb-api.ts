55914448ead3545eb018221d7a0ee408d5128381
TokarevIP <tokarevrab@gmail.com>
2024-05-11 19:43:35 +0100

    Add getting TV Series api function

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -69,3 +69,16 @@ export const getUpcomingMovies = () => {
         .then(res => res.json())
         .then(json => json.results);
 };
+
+export const getPopularTvSeries = () => {
+    return fetch(
+        `https://api.themoviedb.org/3/tv/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=ru&page=1`
+    ).then((response) => {
+        if (!response.ok)
+            throw new Error(`Unable to fetch popular tv series. Response status: ${response.status}`);
+        return response.json();
+    })
+        .catch((error) => {
+            throw error
+        });
+};
